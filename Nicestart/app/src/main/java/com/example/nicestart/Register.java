30a4e67f15ee3f3d6b79ff1c5f8c1659ba30f537
“Ignacio <ignacio.rc1@gmail.com>
2023-11-06 13:21:43 +0100

    añadido vista register

--- Nicestart/app/src/main/java/com/example/nicestart/Register.java
+++ Nicestart/app/src/main/java/com/example/nicestart/Register.java
@@ -1,25 +1,22 @@
 package com.example.nicestart;
 
 import androidx.appcompat.app.AppCompatActivity;
-import android.view.View;
+
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
-public class Login extends AppCompatActivity {
+public class Register extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_register);
     }
     public void openMain(View v){
-        Intent intent = new Intent(Login.this, Main.class);
+        Intent intent = new Intent(Register.this, Main.class);
         intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
         intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
-        startActivity(intent);
-    }
-    public void openSignup (View v){
-        Intent intent = new Intent(Login.this,Register.class);
         startActivity(intent);
     }
 }
