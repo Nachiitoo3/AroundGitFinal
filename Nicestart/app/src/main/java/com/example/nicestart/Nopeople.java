0e31be46845b8f753c1bf72a00253a514eacdd44
“Ignacio <ignacio.rc1@gmail.com>
2023-11-20 13:29:06 +0100

    vista Nopeople con accion SwipeRefresh.

---
+++ Nicestart/app/src/main/java/com/example/nicestart/Nopeople.java
@@ -0,0 +1,39 @@
+package com.example.nicestart;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
+
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.widget.Toast;
+
+public class Nopeople extends AppCompatActivity {
+    private WebView miVisorWeb;
+    private SwipeRefreshLayout swipeLayout;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_nopeople);
+
+        swipeLayout = (SwipeRefreshLayout) findViewById(R.id.myswipe);
+        swipeLayout.setOnRefreshListener(mOnRefreshListener);
+
+        miVisorWeb = (WebView) findViewById(R.id.vistaweb);
+        WebSettings webSettings = miVisorWeb.getSettings();
+        webSettings.setLoadWithOverviewMode(true);
+        webSettings.setUseWideViewPort(true);
+        miVisorWeb.loadUrl("https://thispersondoesnotexist.com");
+    }
+    protected SwipeRefreshLayout.OnRefreshListener
+        mOnRefreshListener = new SwipeRefreshLayout.OnRefreshListener() {
+        @Override
+        public void onRefresh() {
+            Toast toast0 = Toast.makeText(Nopeople.this,"Hi there! I don´t exist :)",Toast.LENGTH_LONG);
+            toast0.show();
+            miVisorWeb.reload();
+            swipeLayout.setRefreshing(false);
+        }
+    };
+}
