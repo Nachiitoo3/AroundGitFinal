4ccd1b1c283fdc97713fb0d4ae5eeb376a6f1704
“Ignacio <ignacio.rc1@gmail.com>
2023-12-20 13:12:02 +0100

    Incluyendo menu_main y snackbars

--- Nicestart/app/src/main/java/com/example/nicestart/Main.java
+++ Nicestart/app/src/main/java/com/example/nicestart/Main.java
@@ -2,11 +2,13 @@ package com.example.nicestart;
 
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
 import android.content.Intent;
 import android.media.Rating;
 import android.os.Bundle;
 import android.view.ContextMenu;
+import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
@@ -15,6 +17,8 @@ import android.widget.Button;
 import android.widget.RatingBar;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import com.google.android.material.snackbar.Snackbar;
 
 public class Main extends AppCompatActivity {
 
@@ -41,25 +45,67 @@ public class Main extends AppCompatActivity {
 
     }
 
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        if (id == R.id.camera) {
+            Toast toast = Toast.makeText(this, "going APPBAR CAMERA", Toast.LENGTH_LONG);
+            toast.show();
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+
     public void onCreateContextMenu(ContextMenu menu, View v,
                                     ContextMenu.ContextMenuInfo menuInfo) {
         super.onCreateContextMenu(menu, v, menuInfo);
         MenuInflater inflater = getMenuInflater();
         inflater.inflate(R.menu.menu_context, menu);
     }
-    @Override
+
     public boolean onContextItemSelected(MenuItem item) {
-        if (item.getItemId()==R.id.item) {
-            Toast toast = Toast.makeText(this, "COPIADO EN PORTAPAPELES",
-                    Toast.LENGTH_LONG);
-            toast.show();
+
+        if (item.getItemId() == R.id.item) {
+
+
+
+              final ConstraintLayout mLayout = findViewById(R.id.MyMainConstraint);
+
+              Snackbar snackbar = Snackbar
+                      .make(mLayout, "fancy a Snack while you refresh?", Snackbar.LENGTH_LONG)
+                      .setAction("UNDO", new View.OnClickListener() {
+                          @Override
+                          public void onClick(View view) {
+                              Snackbar snackbar1 = Snackbar.make(mLayout, "Action is restored!", Snackbar.LENGTH_SHORT);
+                              snackbar1.show();
+                        }
+                    });
+              snackbar.show();
+
             return true;
-        } else if (item.getItemId()==R.id.action_settings) {
-            Toast toast2 = Toast.makeText(this,"DESCARGADO",
-                    Toast.LENGTH_LONG );
-            toast2.show();
+        } else if (item.getItemId() == R.id.action_settings) {
+
+
             return true;
-        }else
-                return super.onContextItemSelected(item);
-        }
+
+        } else
+//           return super.onContextItemSelected(item);
+            return false;
     }
+}
+
+
+
